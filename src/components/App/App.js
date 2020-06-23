5fdbd21ca560cbfb12f5f34c1b716088e432603c
zharkowski <maxim199919@rambler.ru>
2020-06-23 05:07:21 +0300

    eslint added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import Main from "../../pages/Main/Main.jsx";
 import RepoCard from "../../pages/RepoCard/RepoCard.jsx";
-import {Switch, Route, Redirect} from 'react-router-dom';
 
 const App = () => (
 	<Switch>
-		{/*<Route path="/home" component={Main} />*/}
-		{/*<Route path="/repo/:repoId" component={RepoCard} />*/}
 		<Route path="/home" render={(props) => (<Main {...props} />)} />
 		<Route path="/repo/:id" render={(props) => (<RepoCard {...props} />)} />
 
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Search = (props) => {
+const Search = () => {
 
 	const searchRepos = (evt) => {
 		evt.preventDefault();
